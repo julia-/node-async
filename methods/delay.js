afd277561400c29c6391abbd061e47f223b0b47e
julia- <julia.ryanx@gmail.com>
2017-12-05 12:09:12 +1100

    add delay parallel function

--- methods/delay.js
+++ methods/delay.js
@@ -15,25 +15,36 @@ console.time('delay 400ms')
 console.time('delay 400ms+500ms')
 console.time('delay 400ms+500ms+700ms')
 
+// const delay400 = delay(400)
+
+// delay400.then(() => {
+//   // After the first delay has finshed
+//   console.timeEnd('delay 400ms')
+
+//   // delay500 starts
+//   console.time('delay 500ms')
+//   const delay500 = delay(500)
+//   delay500.then(() => {
+//     console.timeEnd('delay 500ms')
+//     console.timeEnd('delay 400ms+500ms')
+
+//     // delay700 starts
+//     console.time('delay 700ms')
+//     const delay700 = delay(700)
+//     delay700.then(() => {
+//       console.timeEnd('delay 700ms')
+//       console.timeEnd('delay 400ms+500ms+700ms')
+//     })
+//   })
+// })
+
 const delay400 = delay(400)
+const delay500 = delay(500)
+const delay700 = delay(700)
 
-delay400.then(() => {
-  // After the first delay has finshed
-  console.timeEnd('delay 400ms')
+console.time('parallel');
 
-  // delay500 starts
-  console.time('delay 500ms')
-  const delay500 = delay(500)
-  delay500.then(() => {
-    console.timeEnd('delay 500ms')
-    console.timeEnd('delay 400ms+500ms')
-
-    // delay700 starts
-    console.time('delay 700ms')
-    const delay700 = delay(700)
-    delay700.then(() => {
-      console.timeEnd('delay 700ms')
-      console.timeEnd('delay 400ms+500ms+700ms')
-    })
-  })
+Promise.all([delay400, delay500, delay700])
+  .then(() => {
+    console.timeEnd('parallel')
 })
