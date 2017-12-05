fef9664fd37243293cfc8718845e3dfc3464091b
julia- <julia.ryanx@gmail.com>
2017-12-05 11:45:11 +1100

    add multiple delays in sequence

--- methods/delay.js
+++ methods/delay.js
@@ -1,6 +1,6 @@
 function delay(duration) {
   // Do magic with `new Promise` and `setTimeout`
-  return new Promise((succeed) => {
+  return new Promise((succeed, fail) => {
     // We do our work, which accepts a callback function
     setTimeout(
       // The function that is called after a timeout of `duration`
@@ -11,9 +11,29 @@ function delay(duration) {
   })
 }
 
-console.time('delay 2000ms')
-const aPromise = delay(2000)
+console.time('delay 400ms')
 
-aPromise.then(() => {
-  console.timeEnd('delay 2000ms')
+const delay400 = delay(400)
+
+delay400.then(() => {
+  // After the first delay has finshed
+  console.timeEnd('delay 400ms')
+
+  // delay500 starts
+  console.time('delay 500ms')
+  // console.time('delay 400ms+500ms')
+  const delay500 = delay(500)
+  delay500.then(() => {
+    console.timeEnd('delay 500ms')
+    // console.timeEnd('delay 400ms+500ms')
+
+    // delay700 starts
+    console.time('delay 700ms')
+    // console.time('delay 400ms+500ms+700ms')
+    const delay700 = delay(700)
+    delay700.then(() => {
+      console.timeEnd('delay 700ms')
+      // console.timeEnd('delay 400ms+500ms+700ms')
+    })
+  })
 })
